83c08bc95d3715b86b97fefeef3440f6ad69df6a
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-05-19 14:56:55 -0500

    wip

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	// import other necessary packages
+)
+
+func parseCommand(command string) (string, []string) {
+	// Implement this function according to your design.
+}
+
+func runCommand(command string, arguments []string) string {
+	// Implement this function according to your design.
+}
+
+func main() {
+	// This will be the entry point of your program.
+	fmt.Println("Todo List Application")
+	// Add more code here to handle the commands
+}
